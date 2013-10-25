9bdf45017ac826acf311bd6464109cacd8ded4d8
Ryan Trauntvein <djdefi@gmail.com>
2013-10-25 08:55:01 -0700

    Fixed oops

--- monitor.php
+++ monitor.php
@@ -6,7 +6,7 @@ $result = mysqli_query($mysqli,"SELECT * FROM domains");
 
 while($row = mysqli_fetch_array($result))
   {
-  echo $row['DomainName'] . " " . pingDomain($row['DomainName']) . "" . ms . " " . ;
+  echo $row['DomainName'] . " " . pingDomain($row['DomainName']) . "" . ms;
   echo "<br>";
   }
 
